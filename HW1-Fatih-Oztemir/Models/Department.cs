d8d0c8fe973b9a44f10e25b6cc6c7c5d1a567715
fatih <fatih@DESKTOP-PT432LC>
2021-09-24 15:38:13 +0300

    models created

---
+++ HW1-Fatih-Oztemir/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW1_Fatih_Oztemir.Models
+{
+    public class Department
+    {
+        [Required(ErrorMessage = "Please enter id")]
+        public int Id { get; set; }
+        [StringLength(2, ErrorMessage ="Please enter the valid length(2,20) Department name",MinimumLength =20)]
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ HW1-Fatih-Oztemir/Models/Person .cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW1_Fatih_Oztemir.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Please enter id")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "please enter the your name")]
+        [MinLength(3, ErrorMessage = "your name must be more than 2 letters")]
+        [MaxLength(20, ErrorMessage = "your name must be less than 20 letters")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "please enter the your surname")]
+        [MinLength(3, ErrorMessage = "your surname must be more than 2 letters")]
+        [MaxLength(20, ErrorMessage = "your surname must be less than 20 letters")]
+        public string Surname { get; set; }
+        public int DepartmentId { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
